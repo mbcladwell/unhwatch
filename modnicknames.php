6bedf815de82fd28cb3047d1f098db2a4f8ce399
mbcladwell <mbcladwell@gmail.com>
2024-05-01 14:26:24 -0400

    nicknames.txt

--- modnicknames.php
+++ modnicknames.php
@@ -1,26 +1,42 @@
 <?php
 
+//for each name put the name nickname pair on one line
+//James Jim, Jimmy, Jimbo ==>
+//James Jim
+//James Jimmy
+//James Jimbo
 
 
 $file_name = "nicknames.csv";
+$out = "nicknamesmod.txt";
 $d = file_get_contents($file_name);
 
+$myfile = fopen($out, "a") or die("Unable to open file!");
 
 
 if (($handle = fopen($file_name, "r")) !== false) {
 
-    // loop through each row. The table has 4 columns. I want col 0
-       while (($data = fgetcsv($handle, 0, "\t")) !== false) {
+    while (($data = fgetcsv($handle, 0, "\t")) !== false) {
 
-    $data = fgetcsv($handle, 0, "\t");
+        $data = fgetcsv($handle, 0, "\t");
 
-    echo $data[0]. "\n";
-        echo $data[1]  . "\n\n";
-        
+        $nicks = explode(", ", $data[1] );
+        $n = count($nicks);
+        print_r($nicks);
+        $i = 0;
+        while ($i < $n){
+        $txt = $data[0] .  "\t" . $nicks[$i] .  "\n";
+        fwrite($myfile, $txt);
+            
+            $i++;
         }
+        
+    }
 
     // close file
     fclose($handle);
+    fclose($myfile);
+    
 }
 
 
